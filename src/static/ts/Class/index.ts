100dc7aa35d6209fe31fdd42daa3c7b6d41f00e7
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-05-31 19:10:57 -0300

    The lever can move and change css in the modal

--- src/static/ts/Class/index.ts
+++ src/static/ts/Class/index.ts
@@ -1,8 +1,6 @@
 import { Game, iGame } from './Game.js'
 import { actionTypes } from './reducerSnake.js'
 
-
-
 const lever: HTMLElement = document.getElementById('lever')
 const base: HTMLElement = document.getElementById('base')
 
@@ -10,7 +8,7 @@ const snakeKeys: Array<string> = Object
   .keys(actionTypes)
   .map(key => actionTypes[key])
 
-const scale: number = 10
+const scale: number = 20
 
 const game: iGame = new Game(scale)
 
@@ -19,21 +17,24 @@ window.addEventListener("keydown", ({key}) => {
 
   snakeKeys.some(move => move === key) && game.turn(key)
 
-  if(key === 'ArrowRight'){
-    lever.style.transform = "rotate(45deg) translateX(10px)"
-    base.style.transform = "rotate(315deg) translateX(-13px) translateY(-10px)"
-    setTimeout(()=> {
-      lever.style.transform = ""
-      base.style.transform = ""
-    }, 200)
+  switch(key){
+    case 'ArrowRight':
+      lever.style.transform = "rotate(45deg) translateX(10px)"
+      base.style.transform = "rotate(315deg) translateX(-13px) translateY(-10px)"
+      break
+    case 'ArrowLeft':
+      lever.style.transform = "rotate(-45deg) translateX(-10px)"
+      base.style.transform = "rotate(45deg) translateX(-16px) translateY(16px)"
+      break
+    case 'ArrowDown':
+    case 'ArrowUp':
+      lever.style.setProperty('--ball-down', '-18px')
+      break
+    default:
   }
-
-  if(key === 'ArrowLeft'){
-    lever.style.transform = "rotate(-45deg) translateX(-10px)"
-    base.style.transform = "rotate(45deg) translateX(-16px) translateY(16px)"
-    setTimeout(()=> {
-      lever.style.transform = ""
-      base.style.transform = ""
-    }, 200)
-  }
+  setTimeout(()=> {
+    lever.style.transform = ""
+    base.style.transform = ""
+    lever.style.setProperty('--ball-down', '-31px')
+  }, 200)
 })
