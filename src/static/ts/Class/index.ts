b0cb25fe365f43c5554348c1cac76b926a552d6a
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-05-30 23:39:53 -0300

    the lever move left and right

--- src/static/ts/Class/index.ts
+++ src/static/ts/Class/index.ts
@@ -1,6 +1,11 @@
 import { Game, iGame } from './Game.js'
 import { actionTypes } from './reducerSnake.js'
 
+
+
+const lever: HTMLElement = document.getElementById('lever')
+const base: HTMLElement = document.getElementById('base')
+
 const snakeKeys: Array<string> = Object
   .keys(actionTypes)
   .map(key => actionTypes[key])
@@ -11,5 +16,24 @@ const game: iGame = new Game(scale)
 
 window.addEventListener("keydown", ({key}) => {
   key === 'p' && game.pauseGame()
+
   snakeKeys.some(move => move === key) && game.turn(key)
+
+  if(key === 'ArrowRight'){
+    lever.style.transform = "rotate(45deg) translateX(10px)"
+    base.style.transform = "rotate(315deg) translateX(-13px) translateY(-10px)"
+    setTimeout(()=> {
+      lever.style.transform = ""
+      base.style.transform = ""
+    }, 200)
+  }
+
+  if(key === 'ArrowLeft'){
+    lever.style.transform = "rotate(-45deg) translateX(-10px)"
+    base.style.transform = "rotate(45deg) translateX(-16px) translateY(16px)"
+    setTimeout(()=> {
+      lever.style.transform = ""
+      base.style.transform = ""
+    }, 200)
+  }
 })
