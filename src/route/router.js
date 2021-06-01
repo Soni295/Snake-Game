b10ea6c87e037965d35299192cd379b6a5aef5d0
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-05-31 22:13:03 -0300

    minifiles

--- src/route/router.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const { join } = require('path')
-const STATIC = require('../PATH')
-
-router.get('/', (req, res) => {
-  res.sendFile(join(STATIC, 'index.html'))
-})
-
-module.exports = router
